1996dec597764e21e3733952b2e51ebc2cc9d7a1
VSat08 <satyam2001verma@gmail.com>
2024-02-24 02:20:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import { About } from "./Components/About";
 import Developer from "./Components/Developer";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/Star-Wars-Planets-Directory/" element={<Home />} />
-          <Route path="/directory" element={<Directory />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/dev" element={<Developer />} />
-        </Routes>
-      </BrowserRouter>
+      
+      <Routes>
+        <Route path="/Star-Wars-Planets-Directory/" element={<Home />} />
+        <Route path="/directory" element={<Directory />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dev" element={<Developer />} />
+      </Routes>
     </div>
   );
 }
